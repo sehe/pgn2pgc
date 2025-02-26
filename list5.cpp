583873fcf5653cd67d8ac2bf7b4e715a1b00e315
sehe <sgheeren@gmail.com>
2025-02-26 03:15:55 +0100

    refactor tests

--- list5.cpp
+++ list5.cpp
@@ -1,1 +1,27 @@
 #include "list5.h"
+
+#include <iostream>
+int main() {
+  List<int> l;
+
+  l.add(0);
+  l.add(1);
+  l.add(2);
+  l.remove(1);
+  l.add(3);
+
+  for (unsigned i = 0; i < l.size(); ++i)
+    std::cout << "\n" << l[i];
+
+  if (l.size())
+    l.remove(l.size() - 1);
+
+  for (unsigned i = 0; i < l.size(); ++i)
+    std::cout << "\n" << l[i];
+
+  l.add(4);
+  l.remove(0);
+
+  for (unsigned i = 0; i < l.size(); ++i)
+    std::cout << "\n" << l[i];
+}
---
+++ pqueue_2.cpp
@@ -0,0 +1,47 @@
+#include "pqueue_2.h"
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  PriorityQueue<int> l;
+
+  assert(l.isEmpty());
+  for (int i = 200; i > 1; --i) {
+    l.add(rand());
+    assert(!l.isEmpty());
+  }
+
+  int val = -2;
+  std::cout << std::endl << std::endl;
+  while (l.next(&val)) {
+    std::cout << "\t" << val;
+  }
+
+  PriorityQueue<int> l2(l);
+
+  l.gotoFirst();
+  assert(!l2.next(&val));
+
+  std::cout << std::endl << std::endl;
+  while (l.next(&val)) {
+    std::cout << "\t" << val;
+  }
+
+  l.add(-1);
+
+  l.gotoFirst();
+  l2.gotoFirst();
+  l.next(&val);
+  std::cout << "\nFirst L: " << val;
+
+  l2.next(&val);
+  std::cout << "\nFirst L2: " << val;
+
+  l2 = l;
+  l2.next(&val);
+  std::cout << "\nNext L2: " << val;
+  l.next(&val);
+  std::cout << "\nNext L: " << val;
+}
